29f0e9b493b52fdcf92bbd9516184c986d7be408
Miras Seilkhan <mirasseilkhanov@mail.ru>
2024-12-18 12:01:59 +0500

    Added FaceRecognition v2.0

--- FaceRecognition/FaceRecognitionBase.py
+++ FaceRecognition/FaceRecognitionBase.py
@@ -1,73 +1,51 @@
 import face_recognition
-import imutils
-import pickle
-import time
-import cv2
-import os
- 
-# find path of xml file containing haarcascade file 
-cascPathface = os.path.dirname(
- cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
-# load the harcaascade in the cascade classifier
-faceCascade = cv2.CascadeClassifier(cascPathface)
-# load the known faces and embeddings saved in last file
-data = pickle.loads(open('face_enc', "rb").read())
- 
-print("Streaming started")
-video_capture = cv2.VideoCapture(0)
-# loop over frames from the video file stream
-while True:
-    # grab the frame from the threaded video stream
-    ret, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray,
-                                         scaleFactor=1.1,
-                                         minNeighbors=5,
-                                         minSize=(60, 60),
-                                         flags=cv2.CASCADE_SCALE_IMAGE)
- 
-    # convert the input frame from BGR to RGB 
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # the facial embeddings for face in input
-    encodings = face_recognition.face_encodings(rgb)
-    names = []
-    # loop over the facial embeddings incase
-    # we have multiple embeddings for multiple fcaes
-    for encoding in encodings:
-       # Compare encodings with encodings in data["encodings"]
-       # Matches contain array with boolean values and True for the embeddings it matches closely
-       # and False for rest
-        matches = face_recognition.compare_faces(data["encodings"],
-         encoding)
-        # set name =inknown if no encoding matches
-        name = "Unknown"
-        # check to see if we have found a match
-        if True in matches:
-            #Find positions at which we get True and store them
-            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-            counts = {}
-            # loop over the matched indexes and maintain a count for
-            # each recognized face face
-            for i in matchedIdxs:
-                # Check the names at respective indexes we stored in matchedIdxs
-                name = data["names"][i]
-                # increase count for the name we got
-                counts[name] = counts.get(name, 0) + 1
-            # set name which has highest count
-            name = max(counts, key=counts.get)
- 
- 
-        # update the list of names
-        names.append(name)
-        # loop over the recognized faces
-        for ((x, y, w, h), name) in zip(faces, names):
-            # rescale the face coordinates
-            # draw the predicted face name on the image
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
-             0.75, (0, 255, 0), 2)
-    cv2.imshow("Frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-video_capture.release()
-cv2.destroyAllWindows()
+from PIL import Image, ImageDraw
+
+def face_rec():
+    gal_face_img = face_recognition.load_image_file("img/Miras.jpeg")
+    gal_face_location = face_recognition.face_locations(gal_face_img)
+
+    justice_league_img = face_recognition.load_image_file("img/Adilbek.jpeg")
+    justice_league_faces_locations = face_recognition.face_locations(justice_league_img)
+
+    pil_img1 = Image.fromarray(gal_face_img)
+    draw1 = ImageDraw.Draw(pil_img1)
+
+    for(top, right, bottom, left) in gal_face_location:
+        draw1.rectangle((left, top), (right,bottom), outline=(255, 255, 0), width=4)
+    
+    del draw1
+    pil_img1.save("img/new_Miras.jpg")
+
+def extracting_faces(img_path):
+    count = 0
+    faces = face_recognition.load_image_file(img_path)
+    faces_locations = face_recognition.face_locations(faces)
+
+    for face_location in faces_locations:
+        top, right, bottom, left = face_location
+
+        face_img = faces[top:bottom, left:right]
+        pil_img = Image.fromarray(face_img)
+        pil_img.save(f"img/{count}_face_img.jpg")
+        count += 1
+
+    return f"Found {count} face(s) in this photo"
+
+def compare_faces(img1_path, img2_path):
+    img1 = face_recognition.load_image_file(img1_path)
+    img1_encodings = face_recognition.face_encodings(img1)[0]
+    img2 = face_recognition.load_image_file(img2_path)
+    img2_encodings = face_recognition.face_encodings(img2)[0]
+
+    result = face_recognition.compare_faces([img1_encodings], img2_encodings)
+
+
+
+
+def main():
+    # extracting_faces("img/Adilbek.jpeg")
+    compare_faces("img/Miras.jpeg")
+
+if __name__ == "__main__":
+    main()
